154fb5d859fc225b7a9891b4364e8f698915f4ab
John Borell <assaltshaker@yahoo.com>
2015-04-08 09:54:57 -0600

    new warm-up exercise

--- Ap 8 Warm-up.py
+++ Ap 8 Warm-up.py
@@ -1,4 +1,4 @@
-def sleep_in(weekday, vacation):
+""" def sleep_in(weekday, vacation):
     if weekday == True and vacation == False:
         print False
         return False
@@ -7,4 +7,10 @@ def sleep_in(weekday, vacation):
         return True
 sleep_in(False, False)
 sleep_in(True, False)
-sleep_in(False, True)
+sleep_in(False, True) """
+
+def sleep_in(weekday, vacation):
+    return vacation or not weekday
+print sleep_in(False, False)
+print sleep_in(True, False)
+print sleep_in(False, True)
