98988d0e13b4f901b129d68ce89f36bc9209e24c
John Borell <assaltshaker@yahoo.com>
2015-04-13 15:53:21 -0600

    update exercises

--- exercise_4.py
+++ exercise_4.py
@@ -3,10 +3,8 @@ def choose():
     print " Menu \n What would you like to do? \n 1. Write input to file \n 2. Write input to screen \n 3. Quit"
     choice = raw_input("Which do you choose?: ")
     if choice == "1":
-        text = raw_input("Type text here: ")
-        f = open('text.txt', 'a+')
-        f.write(text)
-        f.close
+        with open('text.txt', 'a+') as f: #another way to manipulate the I/O
+            f.write(raw_input("Type text here: "))
         return choose()
     elif choice == "2":
         text = raw_input("Type text here: ")
---
+++ exercise_5.py
@@ -0,0 +1,14 @@
+# Your program will write out 10 strings of random characters (of random length) to a file
+# called exercise_five.dat (to start, Google “python generate random string”). Once the random
+# strings are written out, your program will then read them back in. For each random
+# string, count the number of characters. For example, if my string was ‘oStoHtpuXAipIyfSzGEmECvSppoJkmHsjogPmKkvmqMuDvnohe’,
+# I would go through and count how many
+# times each character appeared. You should then output each character and its count. Your
+# output might look something like this:
+# ***** BEGIN RANDOM STRING *****
+# xGYMSlMHGQAMNrSzXWqphkGntMpyjMoHyRDzaNOcmVtoeAZzcV
+# A ==> 2 D ==> 1 G ==> 3 H ==> 2 M ==> 5 O ==> 1 N ==> 2 Q ==> 1 S ==> 2
+# R ==> 1 W ==> 1 V ==> 2 Y ==> 1 X ==> 1 Z ==> 1 a ==> 1 c ==> 2 e ==> 1
+# h ==> 1 k ==> 1 j ==> 1 m ==> 1 l ==> 1 o ==> 2 n ==> 1 q ==> 1 p ==> 2
+# r ==> 1 t ==> 2 y ==> 2 x ==> 1 z ==> 3
+# *******************************
